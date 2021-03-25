70f29ff8afd516725204d75bae7506d3416ef79c
Frain8 <wiliamwijaya1985@gmail.com>
2021-03-25 22:58:10 +0700

    Finish soal3b

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-username='frain8' # Store the username of local Linux environtment
+dir_path='/home/frain8/Documents/Sisop/Modul_1/soal_shift1/soal3'
+folder_path="$dir_path/$(date +"%d-%m-%Y")"
 limit=23
+
+# Create folder if folder_path not exist
+if [ ! -d $folder_path ]; then
+    echo "`mkdir $folder_path`"
+fi
 
 for ((i=0, dc=0; i < $limit; i++)); do # dc = downloaded file counter
     # Download pictures
@@ -9,11 +15,11 @@ for ((i=0, dc=0; i < $limit; i++)); do # dc = downloaded file counter
     if [ $name -lt 10 ]; then
         name="0$name"
     fi
-    echo "`wget -O Koleksi_$name.jpg https://loremflickr.com/320/240/kitten`"
+    echo "`wget -O $folder_path/Koleksi_$name.jpg https://loremflickr.com/320/240/kitten`"
 
     # Write to log
     _date="$(date +"%A, %d %b %Y, %T WIB")" # Example: Kamis, 25 Mar 2021, 07:05:44 WIB
-    echo "$_date, download gambar ke-$dc." >> /home/"$username"/log/Foto.log
+    echo "$_date, download gambar ke-$dc." >> "$folder_path/Foto.log"
 
     # Compare current image with all of the previous downloaded images
     if [ $i -gt 1 ]; then # Start when at least 1 file is alredy downloaded
@@ -25,16 +31,16 @@ for ((i=0, dc=0; i < $limit; i++)); do # dc = downloaded file counter
             fi
 
             # Compare downloaded image with previous image
-            cmp="$(idiff "Koleksi_$jname.jpg" "Koleksi_$name.jpg")"
+            cmp="$(idiff "$folder_path/Koleksi_$jname.jpg" "$folder_path/Koleksi_$name.jpg")"
             cmp=$(awk -F " " '/PASS/ {print($1)}' <<< $cmp) # If current image is the same as previous image, this var will be empty
 
             # Delete image if the same image is already downloaded
             if [ -n "$cmp" ]; then
-                echo `rm Koleksi_"$name".jpg` # Delete image
+                echo "`rm $folder_path/Koleksi_$name.jpg`" # Delete image
 
                 # Write to log
                 _date="$(date +"%A, %d %b %Y, %T WIB")" # Example: Kamis, 25 Mar 2021, 07:05:44 WIB
-                echo "$_date, gambar ke-$dc dihapus karena adanya kesamaan dengan gambar ke-$temp." >> /home/"$username"/log/Foto.log
+                echo "$_date, gambar ke-$dc dihapus karena adanya kesamaan dengan gambar ke-$temp." >> "$folder_path/Foto.log"
 
                 ((dc--)) # Decrement downloaded file counter
                 break
