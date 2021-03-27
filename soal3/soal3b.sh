1d77eae50f70866f6305aed3189e9932b0c15a54
William Handi Wijaya <52129348+wiliamhw@users.noreply.github.com>
2021-03-27 18:01:15 +0700

    Add soal2 and fix soal3 bugs (#13)
    * Add soal2
    
    * Revert soal3b
    
    * Update readme
    
    Co-authored-by: fadhildimas <75319605+fadhildimas@users.noreply.github.com>

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "`bash soal3a.sh`"
+curr_day="$(date +"%d")"
+
+if [ $(( ($curr_day-1) % 7 )) -eq 0 || $(( ($curr_day-2) % 4 )) -eq 0 ]; then
+    echo "`bash soal3a.sh`"
+fi
+# echo "`bash soal3a.sh`" # For debugging purpose
